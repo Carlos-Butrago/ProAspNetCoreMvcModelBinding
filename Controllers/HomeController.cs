fc6f5b1b01564b857d1a477460c2c1fc34d37d3f
Carlos Eduardo Butrago de Miranda <carlos.butrago@gestaoprojetos.inf.br>
2019-10-18 18:32:41 -0400

    [task-01] - Add informacoes

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProAspNetCoreMvcModelBinding.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+            private IRepository repository;
+            public HomeController(IRepository repository)
+            {
+                this.repository = repository;
+            }
+
+            public ViewResult Index(int id)
+            {
+                return View("Index", repository[id]);
+            }
+        }
+
+    public ViewResult Index2(int id)
+    {
+        return View("Index", repository[id] ?? repository.Pessoa.First());
+    }
+
+    public IActionResult Index3(int? id)
+    {
+        Pessoa pessoa;
+        if (id.HasValue && (pessoa = repository[id.Value]) != null)
+        {
+            return View(pessoa);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    public ViewResult Cadastro()
+    {
+        return View("Cadastro", new Pessoa());
+    }
+
+    [HttpPost]
+    public ViewResult Cadastro(Pessoa pessoa)
+    {
+        return View("Index", pessoa);
+    }
+
+    public ViewResult EnderecoBasico(EnderecoResumido endereco)
+    {
+        return View("EnderecoBasico", endereco);
+    }
+}
+
---
+++ Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProAspNetCoreMvcModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcModelBinding.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<ProAspNetCoreMvcModelBinding.Models.Pessoa> Pessoa { get; }
+        Pessoa this[int id] { get; set; }
+    }
+}
